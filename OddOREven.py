ad26ba8d1d3de094abbe6b3b3917c287d09bd253
sudhakarparthasarathy <70079821+sudhakarparthasarathy@users.noreply.github.com>
2025-02-07 10:08:24 +0530

    sample codes
    Day 3

---
+++ OddOREven.py
@@ -0,0 +1,5 @@
+number = int(input("Type the number:!"))
+if number % 2 ==0:
+  print("THis number is even number")
+else
+  print("This is a odd number!")
---
+++ RollerCoaster.py
@@ -0,0 +1,7 @@
+# Roller Coaster Booking App
+print("Welcome to the Roller Coaster!")
+Height = int(input("whats Your Height:"))
+if Height > 120:
+    print("You are Eligble to Ride RollerCoaster!")
+else:
+    print("You are Not Elible to Ride RollerCoaster!")
